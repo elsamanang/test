4cd0e0e1fcdf5b703b293d727e694333a9ff63fc
elsa manang <manangelsa07@gmail.com>
2022-05-20 16:53:01 +0200

    add table component

---
+++ src/component/table.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Table({data, handelShow}) {
+    return (
+        <div className="content">
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Type #</th>
+                        <th scope="col">Summary</th>
+                        <th scope="col">Private</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Service</th>
+                        <th scope="col">Autor</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(item =>
+                        <tr key={item.entity.data.id} onClick={handelShow}>
+                            <td>{item.entity.data.number}</td>
+                            <td>{item.entity.data.summary}</td>
+                            <td>{item.entity.data.isPrivate ? 
+                                <img src="success.png" className="mx-2" width={'18px'}/>
+                            : <img src="failed.png" className="mx-2" width={'18px'}/>}</td>
+                            <td>{item.entity.data.status.name}</td>
+                            <td>{item.entity.data.service ? item.entity.data.service.name: ''}</td>
+                            <td>{item.entity.data.author.name}</td>
+                            <td>{item.entity.data.createdOn}</td>
+                            <td>{item.entity.data.updatedOn}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
