ee7aeb17cd45251d68c14e68c93cd4311c571ad4
elsa manang <manangelsa07@gmail.com>
2022-05-20 23:05:57 +0200

    make stucture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import Table from './component/table';
+import From from './component/form';
 
 function App() {
   const [item, setItem] = useState([]);
   const [form, setForm] = useState([]);
+  const [col, setCol] = useState(12);
+  const [oneItem, setOneItem] = useState({});
+  const [showform, setShowform] = useState('none');
 
   const getItem = () => {
     fetch('data/items.json', 
@@ -40,7 +45,7 @@ function App() {
       return response.json();
     }).then(
       res => {
-        setForm(res.forms[0].fieldsets)
+        setForm(res.forms[0].fieldsets[0].fields);
       }
     ).catch (
       err => {
@@ -54,9 +59,33 @@ function App() {
     getForm();
   })
 
+  const handelShow = () => {
+    setCol(6);
+    setShowform('block');
+    
+  }
+
+  const handelUnShow = () => {
+    setCol(12);
+    setShowform('none');
+  }
+
+  const handleChange = elm => {
+    setOneItem(elm);
+    console.log(oneItem);
+    handelShow();
+  }
+
   return (
-    <div className='container'>
-      App
+    <div className='content'>
+      <div className='row'>
+        <div className={'col-md-'+ col}>
+          <Table data={item} handelShow={handelShow} handleChange={handleChange} />
+        </div>
+        <div id='form' className={'col-md-'+ col} style={{display: showform}}>
+          <From handelUnShow={handelUnShow} formulaire={form} selected={oneItem} />
+        </div>
+      </div>
     </div>
   );
 }
