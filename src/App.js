995792b5e8e93ec22f9470df7c9abc9fcbc9d2c2
elsa manang <manangelsa07@gmail.com>
2022-05-20 15:41:23 +0200

    call data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [item, setItem] = useState([]);
+  const [form, setForm] = useState([]);
+
+  const getItem = () => {
+    fetch('data/items.json', 
+      {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      }
+    ).then(
+      response => {
+      return response.json();
+    }).then(
+      res => {
+        setItem(res.collection.items)
+      }
+    ).catch (
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  const getForm = () => {
+    fetch('data/form.json', 
+      {
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      }
+    ).then(
+      response => {
+      return response.json();
+    }).then(
+      res => {
+        setForm(res.forms[0].fieldsets)
+      }
+    ).catch (
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  useEffect(() => {
+    getItem();
+    getForm();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      App
     </div>
   );
 }
