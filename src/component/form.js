4d97e9bc0f9c32143984a3a1ad12652a946ceb20
elsa manang <manangelsa07@gmail.com>
2022-05-21 11:08:05 +0200

    check and uncheck

--- src/component/form.js
+++ src/component/form.js
@@ -10,35 +10,42 @@ export default function From({handelUnShow, formulaire, selected}) {
     }
     return (
         <div className="content">
-            <button className='btn btn-light' onClick={handelUnShow} title="close">
-                <img src="failed.png" className="mx-2" width={'18px'}/>
-            </button>
-            {selected == null? <p>Nothing selected</p>: 
-                <form>
-                    {formulaire.map(item => 
-                        item.type == 'select' ?
-                        <div className="form-group" key={item.name}>
-                            <label>{item.displayName}</label>
-                            <select className="form-control m-1" id="exampleFormControlSelect1" value={selected[item.name]} onChange={handleChange}>
-                            {item["x-options"].map(elm =>
-                                <option value={elm.value} key={elm.text+elm.value}>
-                                    {elm.text}
-                                </option>
+            <div className="card">
+                <div className="card-body">
+                    <button className='btn btn-light' onClick={handelUnShow} title="close">
+                        <img src="failed.png" className="mx-2" width={'18px'}/>
+                    </button>
+                    {selected == null? <p>Nothing selected</p>: 
+                        <form>
+                            {formulaire.map(item => 
+                                item.type == 'select' ?
+                                <div className="form-group" key={item.name}>
+                                    <label>{item.displayName}</label>
+                                    <select className="form-control m-1" id="exampleFormControlSelect1" 
+                                        value={selected[item.name]} onChange={handleChange}>
+                                        {item["x-options"].map(elm =>
+                                            <option value={elm.value} key={elm.text+elm.value}>
+                                                {elm.text}
+                                            </option>
+                                        )}
+                                    </select>
+                                </div>
+                                :
+                                <div className="form-group" key={item.name}>
+                                    <label>{item.displayName}</label>
+                                    <input type={item.type}
+                                        value={selected[item.name]}
+                                        onChange={handleChange}
+                                        className={item.type=="checkbox"? "form-check-input m-1": "form-control m-1"} id="exampleInputEmail1" name="name" 
+                                        checked={selected.IsPrivate}
+                                        />
+                                </div>
+                                
                             )}
-                            </select>
-                        </div>
-                        :
-                        <div className="form-group" key={item.name}>
-                            <label>{item.displayName}</label>
-                            <input type={item.type}
-                                value={selected[item.name]}
-                                onChange={handleChange}
-                                className={item.type=="checkbox"? "form-check-input m-1": "form-control m-1"} id="exampleInputEmail1" name="name" />
-                        </div>
-                        
-                    )}
-                </form>
-            }
+                        </form>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
